0a0f07657685041de002d6e90dbd83ddff68396a
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-24 18:31:02 +0300

    Adding routes

--- src/api/client/routerClient.ts
+++ src/api/client/routerClient.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express"
+import LoginController from './controllers/LoginController';
+import SignupController from './controllers/SignupController';
 var routerClient = express.Router()
-routerClient.get("/",(req:Request, res: Response,next:NextFunction) => {
-    res.json({message: "client test"})
-})
+routerClient.use("/", new LoginController().mainRouter)
+routerClient.use("/", new SignupController().mainRouter)
 export default routerClient;
