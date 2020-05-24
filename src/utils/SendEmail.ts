e26edc982465d7f524cbd94826e45f60ec98ee07
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-24 18:10:11 +0300

    Test email

--- src/utils/SendEmail.ts
+++ src/utils/SendEmail.ts
@@ -16,21 +16,22 @@ export default class Utils {
                 rejectUnauthorized: false
             }
         });
+        let nomDomaine = process.env.URL
         let mailOptions = {
             to: email,
-            subject: 'Code confirmation',
+            subject: 'Confirmation Compte',
             html: `    
         <div style="display: flex; align-items: center; justify-content: center; text-align: center;">
 
         <div style="box-shadow: #f0f0f0 4px 2px 2px 1px; border: #f0f0f0 1px solid; height: 400px; width: 400px;">
 
 
-                <h1 style="font-size: xx-large;margin-top: 70px;">May<span style="color: #0AB099;">day</span></h1>
-                <p style="font-size: medium;margin-top: 55px;">Would you like to send an <b>emergency signal</b> ?  </p>
-                <a style="text-decoration: none !important;" href="http://mayday-kaody.herokuapp.com/api/confirmations/${email}/${code}">
+                <h1 style="font-size: xx-large;margin-top: 70px;">MTATITRA</h1>
+                <p style="font-size: medium;margin-top: 55px;">Veuillez cliquer sur ce bouton pour confirmer votre compte</p>
+                <a style="text-decoration: none !important;" href="http://${nomDomaine}/api/confirmations/${email}/${code}">
                     <button
                     style="background-color: #0AB099;font-size: 15px;border: none;border-radius: 0%; color: white; font-weight: 700; padding: 7px; display: inline-block; margin-top: 10px; border-radius: 1px;margin-top: px;">
-                    Confirm emergency
+                    Confirmer
                 </button>
             </a>
             <p style="font-size: small; margin-top: 35px;">If the button don't work enter this code : <b
