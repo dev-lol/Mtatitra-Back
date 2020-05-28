b775c48f276a31ba6697ed0dac1f298727453078
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-28 17:20:15 +0300

    Date Limite

--- src/api/admin/routerAdmin.ts
+++ src/api/admin/routerAdmin.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from "express"
 import AdminController from './controllers/AdminController';
+import DateLimiteController from './controllers/DateLimiteController';
 
 var routerAdmin = express.Router()
  routerAdmin.use("/",new AdminController().mainRouter)
+ routerAdmin.use("/datelimite/", new DateLimiteController().mainRouter)
 
 export default routerAdmin;
