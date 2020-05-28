49fea2e789269785470f6c3ef5b7dc67072060dd
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-28 18:47:45 +0300

    Coursier Controller

--- src/api/coursier/routerCoursier.ts
+++ src/api/coursier/routerCoursier.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express"
-import CoursierController from "./controllers/CoursierController";
+import CoursierController from './controllers/CoursierController';
+
 
 var routerCoursier = express.Router()
 routerCoursier.use("/",new CoursierController().mainRouter)
