29dd6fcdc97b86b50b632bf3727edee264eb0a46
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-28 18:44:53 +0300

    Use Client Controller instead LoginController

--- src/api/client/controllers/ClientController.ts
+++ src/api/client/controllers/ClientController.ts
@@ -1,116 +1,54 @@
-import { Controller } from "../../Controller";
-import { Connection, Repository,createConnection } from "typeorm";
-
-
-import { Client } from "../../../entities/Client";
-import { Router,Response,Request, NextFunction } from "express";
-import { Livraison } from "../../../entities/Livraison";
-import { Produit } from "../../../entities/Produit";
+import { Router, Response, Request, NextFunction, ErrorRequestHandler } from "express";
+import { Controller } from "../../Controller"
+import { Client } from "../../../entities/Client"
+import { Repository, Connection, createConnection } from "typeorm";
 import { ormconfig } from "../../../config";
-export default class ClientController extends Controller{
-    clientRepository : Repository<Client>
-    livraisonRepository : Repository<Livraison>
-    produitRepository : Repository<Produit>
-    constructor(){
+import { jwt } from 'jsonwebtoken';
+export default class ClientController extends Controller {
+    clientRepository: Repository<Client>
+    constructor() {
         super()
         this.createConnectionAndAssignRepository()
-            .then(async(_)=>{
+            .then(async (_) => {
                 await this.addAllRoutes(this.mainRouter)
             })
     }
-    async createConnectionAndAssignRepository() : Promise<void>{
-        let connection : Connection = await createConnection(ormconfig)
-         this.clientRepository =  connection.getRepository(Client)
-        this.livraisonRepository = connection.getRepository(Livraison) 
-        this.produitRepository = connection.getRepository(Produit)
+
+
+    async createConnectionAndAssignRepository(): Promise<any> {
+        let connection: Connection = await createConnection(ormconfig)
+        this.clientRepository = connection.getRepository(Client)
     }
-    async addGet(router : Router){
-        await this.allLivraison(router)
+    async addGet(router: Router): Promise<void> {
+
+    }
+    async addPost(router: Router): Promise<void> {
+        await this.postClient(router)
     }
 
-    async allLivraison(router : Router) : Promise<void>{
-        router.get("/:idClient/livraison",async (req:Request,res:Response,next : NextFunction)=>{
-           
-            let liv =await this.livraisonRepository.find({
-               relations : ["produits","idCliClient"],
-                where : {idCliClient : await this.clientRepository.findOne(req.params.idClient)}
-            })
-            if(liv !==undefined) {
-                this.sendResponse(res,200,{
-                    message : "success",
-                    data : liv
-                })
-            }
-        })
-    }
-    async addPost(router : Router){
-        await this.setLivraison(router)
-   
-    }   /**
-     *
-     *
-     * @param {Router} router
-     * @returns {Promise<void>}
-     * @memberof ClientController
-     */
-    async setLivraison(router: Router) : Promise<void>{
-        router.post("/:idClient/livraison",async(req:Request,res:Response,next : NextFunction)=>{
-
-            try{
-                let livraisonInfo : Livraison = await this.createLivraisonFromRequest(req)
-                    
-                let livraisonInfoSaved : Livraison = await this.saveLivraisonInfoToDatabase(livraisonInfo)
-
-                let produitLivraison : Produit[] = await this.createProduitsFromRequest(req)
-
-                
-                await this.saveLivraisonProduitToDb(produitLivraison,livraisonInfoSaved)
-                    .then(()=>{
-                        this.sendResponse(res,200,{
-                            message : "livraison set "
-                        })
-                    }).catch(err=>{
-                        this.passErrorToExpress(err, next)
+    async postClient(router: Router) {
+        router.post("/login", async (req: Request, res: Response, next: NextFunction) => {
+            let client = await this.clientRepository.findOneOrFail({where: {emailCli: req.body.username}})
+            var bcrypt = require("bcrypt")
+            bcrypt.compare(req.body.password, client.passCli, (err, isSame) => {
+                if (!err && isSame) {
+                    this.sendResponse(res, 200, {
+                        token: jwt.sign({ client: client.emailCli }, process.env.ADMIN_PASS_PHRASE, { expiresIn: "30j" })
                     })
+                } else {
+                    this.sendResponse(res, 401, {
+                        message: "Invalid credentials"
+                    })
+                }
+            })
 
-               
-            }catch(err) {
-                this.passErrorToExpress(err, next)
-            }
-                       
         })
     }
-   
+    async addPut(router: Router): Promise<void> {
 
-    private async isLivraisionInfoSet(liv : Livraison) : Promise<boolean> {
-        return liv !== undefined
     }
-    private async saveLivraisonInfoToDatabase(liv : Livraison) : Promise<Livraison> {
-        return await this.livraisonRepository.save(liv)
+
+    async addDelete(router: Router): Promise<void> {
+
     }
-    private async createLivraisonFromRequest(req:Request) : Promise<Livraison>{
-        let client : Client = await this.clientRepository.findOne(req.params.idClient)
-        let livraison = await  this.livraisonRepository.create(req.body as Object)
-        livraison.idCliClient = client
-
-        return livraison
-    }
-    
-    private async createProduitsFromRequest(req:Request) : Promise<Produit[]>{
-        
-       return req.body.produit
-   }
-   private  async saveLivraisonProduitToDb(pr : Produit[],liv : Livraison) : Promise<void>{
-        
-        return await pr.forEach(prod =>{
-            prod.idLivLivraison = liv
-             this.produitRepository.save(prod)
-            
-       })   
-       
-   }
-    async addDelete(router : Router){}
-    async addPut(router : Router){}
-
-
 }
--- src/api/client/routerClient.ts
+++ src/api/client/routerClient.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response, NextFunction } from "express"
-import LoginController from './controllers/LoginController';
 import SignupController from './controllers/SignupController';
-import ClientController from "./controllers/ClientController";
+import ClientController from "./controllers/LivraisonController";
 var routerClient = express.Router()
-routerClient.use("/", new LoginController().mainRouter)
+routerClient.use("/", new ClientController().mainRouter)
 routerClient.use("/", new SignupController().mainRouter)
-routerClient.use("/",new ClientController().mainRouter)
 export default routerClient;
