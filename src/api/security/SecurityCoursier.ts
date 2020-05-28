8f5c509fc7450108f8242cfddaf89fcac503f090
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-28 18:09:54 +0300

    Basic security test For Admin Client Coursier

---
+++ src/api/security/SecurityCoursier.ts
@@ -0,0 +1,27 @@
+
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+export default function securityCoursier(req: Request, res: Response, next: NextFunction) {
+    var jwtToken: string = req.headers["authorization"]
+    console.log(jwtToken)
+    if (jwtToken == undefined) {
+        res.status(401).send({
+            message: "JWT token Missing"
+        })
+        return;
+    }
+    try {
+        jwt.verify(jwtToken.split(" ")[1], process.env.COURSIER_PASS_PHRASE, (error, payload) => {
+            if (error != null) {
+                res.status(401).send({
+                    message: error.message
+                })
+            }
+        })
+    } catch (error) {
+        res.status(401).send({
+            message: error.message
+        })
+        return;
+    }
+}
