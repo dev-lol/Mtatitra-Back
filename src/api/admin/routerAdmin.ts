5045e6d805f970559d90d42e98ecd45cafc30ded
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-28 17:52:11 +0300

    Types Coursier et Produits

--- src/api/admin/routerAdmin.ts
+++ src/api/admin/routerAdmin.ts
@@ -2,10 +2,14 @@ import express, { Request, Response, NextFunction } from "express"
 import AdminController from './controllers/AdminController';
 import DateLimiteController from './controllers/DateLimiteController';
 import TypeLivraisonController from './controllers/TypeLivraisonController';
+import TypeProduitController from './controllers/TypeProduitController';
+import TypeCoursierController from './controllers/TypeCoursierController';
 
 var routerAdmin = express.Router()
  routerAdmin.use("/",new AdminController().mainRouter)
  routerAdmin.use("/datelimite", new DateLimiteController().mainRouter)
  routerAdmin.use("/typelivraison", new TypeLivraisonController().mainRouter)
+ routerAdmin.use("/typecoursier", new TypeCoursierController().mainRouter)
+ routerAdmin.use("/typeproduit", new TypeProduitController().mainRouter)
 
 export default routerAdmin;
