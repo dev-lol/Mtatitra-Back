5c6912002f9e8e08e3f6b94758ade7d5067d012f
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-05-31 10:56:22 +0300

    Ordre etats

--- src/api/admin/controllers/EtatController.ts
+++ src/api/admin/controllers/EtatController.ts
@@ -4,6 +4,7 @@ import { Etats } from "../../../entities/Etats"
 import { Repository, Connection, createConnection } from "typeorm";
 import { ormconfig } from "../../../config";
 import { runInThisContext } from "vm";
+import { json } from "body-parser";
 export default class EtatsController extends Controller {
     etatsRepository: Repository<Etats>
     constructor() {
@@ -39,7 +40,7 @@ export default class EtatsController extends Controller {
     }
 
     private async fetchEtatssFromDatabase(): Promise<Etats[]> {
-        return await this.etatsRepository.find({where: {estSupprime: false}})
+        return await this.etatsRepository.find({ where: { estSupprime: false } })
     }
     async addPost(router: Router): Promise<void> {
         await this.postEtats(router)
@@ -53,9 +54,9 @@ export default class EtatsController extends Controller {
             let etatsSaved: Etats = await this.saveEtatsToDatabase(etatsToSave)
 
             if (await this.isEtatsSaved(etatsSaved)) {
-                this.sendResponse(res,200,{message: "OK"})
+                this.sendResponse(res, 200, { message: "OK" })
             } else {
-                this.sendResponse(res,400,{message: "KO"})
+                this.sendResponse(res, 400, { message: "KO" })
             }
 
         })
@@ -79,18 +80,53 @@ export default class EtatsController extends Controller {
 
 
     async addPut(router: Router): Promise<void> {
-        router.put("/:idEtats",async (req: Request, res: Response, next: NextFunction) => {
+        router.put("/:idEtats", async (req: Request, res: Response, next: NextFunction) => {
             try {
                 let etats: Etats = await this.etatsRepository.findOneOrFail(Number(req.params.idEtats))
                 etats = this.etatsRepository.merge(etats, req.body as Object)
                 await this.etatsRepository.save(etats)
-                this.sendResponse(res,200, {message: "Etats changed"})
+                this.sendResponse(res, 200, { message: "Etats changed" })
             } catch (error) {
                 console.log(error)
-                this.sendResponse(res,404, {message: "Etats not found"})   
+                this.sendResponse(res, 404, { message: "Etats not found" })
             }
-                
+
         })
+
+        this.patchOrdre(router);
+    }
+    /**
+     *
+     *
+     * @private
+     * @param {Router} router
+     * @memberof EtatsController
+     * 
+     * donnee entree:
+     * 
+     * ordre = {idEta: ordre} 
+     * exemple: {1:3, 2:4}
+     * 
+     */
+    private patchOrdre(router: Router) {
+        router.patch("/ordre", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                var ordre: Object = JSON.parse(req.body.ordre)
+                let count = await this.etatsRepository.count()
+                if (Object.keys(ordre).length != count || (new Set(Object.values(ordre))).size != count)
+                    throw new Error("Ordre dupliquee ou manquante")
+                var etats = await this.etatsRepository.find()
+                for(let etat of etats){
+                    etat.ordreEta = ordre[etat.idEta]
+                }
+                await this.etatsRepository.save(etats);
+                this.sendResponse(res, 200, { message: "Ordre changed" });
+            }
+            catch (error) {
+                console.log(error);
+                this.sendResponse(res, 404, { message: "Ordre not changed" });
+            }
+        });
     }
 
     async addDelete(router: Router): Promise<void> {
@@ -98,11 +134,11 @@ export default class EtatsController extends Controller {
             try {
                 let etats: Etats = await this.etatsRepository.findOneOrFail(Number(req.params.idEtats))
                 await this.etatsRepository.remove(etats)
-                this.sendResponse(res,203, {message: "Etats deleted"})
+                this.sendResponse(res, 203, { message: "Etats deleted" })
             } catch (error) {
-                this.sendResponse(res,404, {message: "Etats not found"})   
+                this.sendResponse(res, 404, { message: "Etats not found" })
             }
-                
+
         })
     }
 }
