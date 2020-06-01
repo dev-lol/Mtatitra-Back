c71baae70bf483e8d0db37187f40e1881a3a7191
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-06-01 18:06:55 +0300

    Tarif Get Client

--- src/api/client/routerClient.ts
+++ src/api/client/routerClient.ts
@@ -2,8 +2,10 @@ import express, { Request, Response, NextFunction } from "express"
 import SignupController from './controllers/SignupController';
 import ClientController from "./controllers/ClientController";
 import LivraisonController from "./controllers/LivraisonController";
+import TarifController from './controllers/TarifController';
 var routerClient = express.Router()
 routerClient.use("/", new ClientController().mainRouter)
 routerClient.use("/", new SignupController().mainRouter)
 routerClient.use("/",new LivraisonController().mainRouter)
+routerClient.use("/tarif",new TarifController().mainRouter)
 export default routerClient;
