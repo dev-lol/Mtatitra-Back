b4b3a3b4f0e19eea789de02fcd709c7398291c82
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-06-23 19:43:05 +0300

    Tarif get by typeCoursier by zone

--- src/api/client/controllers/TarifController.ts
+++ src/api/client/controllers/TarifController.ts
@@ -4,6 +4,7 @@ import { Tarif } from "../../../entities/Tarif"
 import { getRepository } from "typeorm";
 import { ormconfig } from "../../../config";
 import { TypeCoursier } from '../../../entities/TypeCoursier';
+import { Zone } from "../../../entities/Zone";
 export default class TarifController extends Controller {
     constructor() {
         super()
@@ -11,6 +12,7 @@ export default class TarifController extends Controller {
     }
     async addGet(router: Router): Promise<void> {
         await this.getAllTarif(router)
+        await this.getTarif(router)
     }
 
 
@@ -30,14 +32,34 @@ export default class TarifController extends Controller {
 
     }
 
+    private async getTarif(router: Router): Promise<void> {
+        router.get("/typecoursier/:typeCoursier/zone/:zone", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+
+                let tarifs = await getRepository(Tarif).findOneOrFail({
+                    where: {
+                        idTypeCouTypeCoursier: req.params.typeCoursier,
+                        idZonZone: req.params.zone
+                    }
+                })
+                this.sendResponse(res, 200, tarifs)
+            } catch (err) {
+                console.log(err)
+                this.sendResponse(res, 404, { message: "Tarif not found" })
+
+            }
+        })
+
+    }
+
     private async fetchTarifsFromDatabase(): Promise<TypeCoursier[]> {
         return await getRepository(TypeCoursier)
-        .createQueryBuilder("typeCoursier")
-        .innerJoinAndSelect("typeCoursier.tarifs","tarif")
-        .leftJoinAndSelect("tarif.idZonZone", "zone")
-        .where("typeCoursier.estSupprime = false")
-        .where("zone.estSupprime = false")
-        .getMany()
+            .createQueryBuilder("typeCoursier")
+            .innerJoinAndSelect("typeCoursier.tarifs", "tarif")
+            .leftJoinAndSelect("tarif.idZonZone", "zone")
+            .where("typeCoursier.estSupprime = false")
+            .where("zone.estSupprime = false")
+            .getMany()
     }
     async addPost(router: Router): Promise<void> {
     }
