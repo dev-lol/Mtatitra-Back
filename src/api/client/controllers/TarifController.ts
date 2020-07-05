bdef4e2138bbfa29d0fb3e4d94133413a0cae60a
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-07-05 13:01:40 +0300

    feat: tarif get

--- src/api/client/controllers/TarifController.ts
+++ src/api/client/controllers/TarifController.ts
@@ -5,43 +5,37 @@ import { getRepository } from "typeorm";
 import { ormconfig } from "../../../config";
 import { TypeCoursier } from '../../../entities/TypeCoursier';
 import { Zone } from "../../../entities/Zone";
+import ErrorValidator from "../../ErrorValidator";
+import { query } from "express-validator";
 export default class TarifController extends Controller {
     constructor() {
         super()
         this.addAllRoutes(this.mainRouter)
     }
     async addGet(router: Router): Promise<void> {
-        await this.getAllTarif(router)
         await this.getTarif(router)
     }
 
 
-    private async getAllTarif(router: Router): Promise<void> {
-        router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-            try {
-
-                let tarifs = await this.fetchTarifsFromDatabase()
-
-                this.sendResponse(res, 200, tarifs)
-            } catch (err) {
-                console.log(err)
-                this.sendResponse(res, 404, { message: "Tarif not found" })
-
-            }
-        })
-
-    }
-
     private async getTarif(router: Router): Promise<void> {
-        router.get("/typecoursier/:typeCoursier/zone/:zone", async (req: Request, res: Response, next: NextFunction) => {
+        router.get("/", [
+            query(['zoneDepart', 'zoneArrivee', 'typeCoursier']).notEmpty().withMessage("error query"),
+            query('zoneDepart').custom((value, { req }) => value != req.query.zoneArrivee).withMessage("same value"),
+            query('zoneArrivee').custom((value, { req }) => value != req.query.zoneDepart).withMessage("same value")
+        ], ErrorValidator, async (req: Request, res: Response, next: NextFunction) => {
             try {
+                const dep = req.query.zoneDepart
+                const arr = req.query.zoneArrivee
+                const type = req.query.typeCoursier
+                let tarifs = await getRepository(Tarif)
+                    .createQueryBuilder("tarif")
+                    .where("tarif.idTypeCouTypeCoursier = :typeCou", { typeCou: type })
+                    .andWhere("tarif.idZonDepart = :idDepart", { idDepart: dep })
+                    .andWhere("tarif.idZonArrivee = :idArrivee", { idArrivee: arr })
+                    .orWhere("tarif.idZonDepart = :idDepart2", { idDepart2: arr })
+                    .andWhere("tarif.idZonArrivee = :idArrivee2", { idArrivee2: dep })
+                    .getOne()
 
-                let tarifs = await getRepository(Tarif).findOneOrFail({
-                    where: {
-                        idTypeCouTypeCoursier: req.params.typeCoursier,
-                        idZonZone: req.params.zone
-                    }
-                })
                 this.sendResponse(res, 200, tarifs)
             } catch (err) {
                 console.log(err)
@@ -50,16 +44,6 @@ export default class TarifController extends Controller {
             }
         })
 
-    }
-
-    private async fetchTarifsFromDatabase(): Promise<TypeCoursier[]> {
-        return await getRepository(TypeCoursier)
-            .createQueryBuilder("typeCoursier")
-            .innerJoinAndSelect("typeCoursier.tarifs", "tarif")
-            .leftJoinAndSelect("tarif.idZonZone", "zone")
-            .where("typeCoursier.estSupprime = false")
-            .where("zone.estSupprime = false")
-            .getMany()
     }
     async addPost(router: Router): Promise<void> {
     }
