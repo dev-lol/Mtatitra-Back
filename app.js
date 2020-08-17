acf58394fb78826b07f3a46e81268e22a792c583
Rajaomaria Jaona <26646864+rajaomariajaona@users.noreply.github.com>
2020-08-17 20:29:35 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("OK");
+const app = require("express");
+const join = require("path").join
+app.get("/",(req,res) => {
+  res.sendFile(join(__dirname,"Mtatitra-Front/dist/Mtatitra/foo.zip"));
+})
